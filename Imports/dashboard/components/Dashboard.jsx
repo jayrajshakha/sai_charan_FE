e524442b30d6416c9bfff5ca54169bcf2a6ed1b8
jayrajshakha <jayrajshakha01@gmail.com>
2024-09-28 18:05:02 +0530

    fixed toggle issue 2.o

--- Imports/dashboard/components/Dashboard.jsx
+++ Imports/dashboard/components/Dashboard.jsx
@@ -1,27 +1,14 @@
 "use client";
 import React from "react";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
-import LogOutBox from "../../../components/helper/LogOutBox";
-// import { BillStore } from "@/data/BillStore";
-// import { useSelector } from "react-redux";
 
 const Dashsboard = () => {
   const router = useRouter();
-  const [toggle, setToggle] = useState(false);
-
-  //   const UserData = useSelector((state) => state.UserData.value);
 
   return (
-    <div className={`  ${toggle ? "h-[80vh]" : "h-full"}  p-2`}>
+    <div className={`h-full p-2`}>
       <header className="flex justify-between items-center py-4 px-6 bg-white rounded-md shadow-md">
         <h1 className="text-3xl font-bold text-gray-800">Dashboard</h1>
-        <button
-          onClick={() => setToggle(true)}
-          className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700"
-        >
-          Logout
-        </button>
       </header>
       <main className="  mt-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         <section
@@ -40,10 +27,7 @@ const Dashsboard = () => {
           <p className="mt-2 text-gray-600">Some Form content...</p>
         </section>
 
-        <section
-          onClick={() => router.push("/admin/records")}
-          className="p-4 bg-white rounded-md shadow-md cursor-pointer hover:scale-110 transition-all duration-200"
-        >
+        <section className="p-4 bg-white rounded-md shadow-md cursor-pointer hover:scale-110 transition-all duration-200">
           <h2 className="text-2xl font-semibold text-gray-800">Records</h2>
           <p className="mt-2 text-gray-600">Some Records content...</p>
         </section>
@@ -59,7 +43,6 @@ const Dashsboard = () => {
           <p className="mt-2 text-gray-600">Some settings content...</p>
         </section>
       </main>
-      {toggle && <LogOutBox setToggle={setToggle} />}
     </div>
   );
 };
